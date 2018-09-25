0375285a474c97b7128c62f28becd511f8f9d53f
Ryan James McGill <ryanjamesmcgill@gmail.com>
2018-09-24 21:16:54 -0700

    leds working

--- vf104v1/vf104v1.ino
+++ vf104v1/vf104v1.ino
@@ -1,83 +1,135 @@
-/* Encoder Library - Basic Example
- * http://www.pjrc.com/teensy/td_libs_Encoder.html
- *
- * This example code is in the public domain.
- */
-
 #include <Encoder.h>
+#include <JC_Button.h>
 
-int ENCODER_PIN_A = 18;
-int ENCODER_PIN_B = 19;
-int LED_PINS[] = {2,3,4,5,6,7,8,9,10,11,12,13};
-int SWITCH_PIN = 53;
-int TAP_PIN = 21;
-volatile byte TAP_STATE = HIGH; 
-long DEBOUCE_DELAY = 50;
-long LAST_DEBOUNCE_TIME = 0;
-Encoder myEnc(ENCODER_PIN_A, ENCODER_PIN_B);
+int ENCODER_A_PIN = 18;
+int ENCODER_B_PIN = 19;
+Encoder ENCODER(ENCODER_B_PIN, ENCODER_A_PIN);
+long ENCODER_POSITION = -999;
+int ENCODER_MAX_POSITION = 127;
+int ENCODER_MIN_POSITION = 0;
+int LED_PARAM_PINS[] = {32, 33, 34, 35, 36, 37, 38, 39};
+int LED_PARAM_PINS_LENGTH = 8;
+int LED_SAVE_PIN = 29;
+int LED_ENCODER_PINS[] = {A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, 44, 45, 46};
+int LED_ENCODER_PINS_LENGTH = 15;
+float LED_ENCODER_STEP_POSITION = (ENCODER_MAX_POSITION - ENCODER_MIN_POSITION) / LED_ENCODER_PINS_LENGTH;
+int LED_CURVE[] = {
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01,
+0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02,
+0x02, 0x02, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x04, 0x04, 0x04, 0x04, 0x04, 0x05, 0x05, 0x05,
+0x05, 0x06, 0x06, 0x06, 0x07, 0x07, 0x07, 0x08, 0x08, 0x08, 0x09, 0x09, 0x0A, 0x0A, 0x0B, 0x0B,
+0x0C, 0x0C, 0x0D, 0x0D, 0x0E, 0x0F, 0x0F, 0x10, 0x11, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1F, 0x20, 0x21, 0x23, 0x24, 0x26, 0x27, 0x29, 0x2B, 0x2C,
+0x2E, 0x30, 0x32, 0x34, 0x36, 0x38, 0x3A, 0x3C, 0x3E, 0x40, 0x43, 0x45, 0x47, 0x4A, 0x4C, 0x4F,
+0x51, 0x54, 0x57, 0x59, 0x5C, 0x5F, 0x62, 0x64, 0x67, 0x6A, 0x6D, 0x70, 0x73, 0x76, 0x79, 0x7C,
+0x7F, 0x82, 0x85, 0x88, 0x8B, 0x8E, 0x91, 0x94, 0x97, 0x9A, 0x9C, 0x9F, 0xA2, 0xA5, 0xA7, 0xAA,
+0xAD, 0xAF, 0xB2, 0xB4, 0xB7, 0xB9, 0xBB, 0xBE, 0xC0, 0xC2, 0xC4, 0xC6, 0xC8, 0xCA, 0xCC, 0xCE,
+0xD0, 0xD2, 0xD3, 0xD5, 0xD7, 0xD8, 0xDA, 0xDB, 0xDD, 0xDE, 0xDF, 0xE1, 0xE2, 0xE3, 0xE4, 0xE5,
+0xE6, 0xE7, 0xE8, 0xE9, 0xEA, 0xEB, 0xEC, 0xED, 0xED, 0xEE, 0xEF, 0xEF, 0xF0, 0xF1, 0xF1, 0xF2,
+0xF2, 0xF3, 0xF3, 0xF4, 0xF4, 0xF5, 0xF5, 0xF6, 0xF6, 0xF6, 0xF7, 0xF7, 0xF7, 0xF8, 0xF8, 0xF8,
+0xF9, 0xF9, 0xF9, 0xF9, 0xFA, 0xFA, 0xFA, 0xFA, 0xFA, 0xFB, 0xFB, 0xFB, 0xFB, 0xFB, 0xFB, 0xFC,
+0xFC, 0xFC, 0xFC, 0xFC, 0xFC, 0xFC, 0xFC, 0xFC, 0xFD, 0xFD, 0xFD, 0xFD, 0xFD, 0xFD, 0xFD, 0xFD,
+0xFD, 0xFD, 0xFD, 0xFD, 0xFD, 0xFD, 0xFD, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFF, 0xFF
+};
+int PARAM_BUTTON_PIN = 30;
+
+int LAST_PARAM_BUTTON_VALUE = 0;
+char STATE_BANK = 'A'; // A,B,C indicating current preset bank
+int STATE_PRESET = 1; // 1-3 indicating current preset
+int STATE_PARAMETER = 1; // 1-8 indicating current parameter
+bool STATE_SAVE_INDICATOR = false; // indicates if save led should be blinking
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Basic Encoder Test 2:");
-  pinMode(SWITCH_PIN, INPUT_PULLUP);
-  pinMode(TAP_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(TAP_PIN), tap, FALLING);
+  intializePins();
+  turnOnLeds();
 }
-
-long oldPosition  = -999;
-volatile byte state = LOW;
 
 void loop() {
-  long newPosition = myEnc.read();
-  if (newPosition != oldPosition) {
-    if (newPosition > 127) {
-      myEnc.write(127);
-      newPosition = 127;
-    } else if (newPosition < 0) {
-      myEnc.write(0);
-      newPosition = 0;
+  updateEncoderPosition();
+  updateStates();
+  
+  setEncoderLeds();
+  setParameterLeds();
+}
+
+void updateStates() {
+  // update bank
+  // update preset
+  // update parameter
+  int currentParamButtonValue = digitalRead(PARAM_BUTTON_PIN);
+  if (LAST_PARAM_BUTTON_VALUE == 1 && currentParamButtonValue == 0) {
+    nextParam();
+  }
+  LAST_PARAM_BUTTON_VALUE = currentParamButtonValue;
+}
+
+void nextParam() {
+  STATE_PARAMETER = STATE_PARAMETER + 1;
+  if (STATE_PARAMETER > LED_PARAM_PINS_LENGTH) {
+    STATE_PARAMETER = 1;
+  }
+}
+
+void setParameterLeds() {
+  for (int idx = 0; idx < LED_PARAM_PINS_LENGTH; idx++) {
+    if (idx + 1 == STATE_PARAMETER) {
+      digitalWrite(LED_PARAM_PINS[idx], HIGH);
+    } else {
+      digitalWrite(LED_PARAM_PINS[idx], LOW);
     }
-    oldPosition = newPosition;
-    //Serial.println(newPosition);
   }
-  
-  if (digitalRead(SWITCH_PIN) == LOW){
-    setLeds(0, LED_PINS, 12);
+}
+
+void setEncoderLeds() {
+ for (int idx = 0; idx < LED_ENCODER_PINS_LENGTH; idx++) {
+  float max_position = (idx+1) * LED_ENCODER_STEP_POSITION;
+  float min_position = idx * LED_ENCODER_STEP_POSITION;
+  setEncoderLed(LED_ENCODER_PINS[idx], ENCODER_POSITION, min_position, max_position);
+ }
+}
+
+void setEncoderLed(int pin, long encoder_position, float led_min_posiiton, float led_max_positon) {
+  int ledBrightness;
+  int ledMaxBrghtness = 255;
+  float fraction;
+  if (encoder_position >= led_max_positon) {
+    ledBrightness = LED_CURVE[ledMaxBrghtness];
+  } else if (encoder_position <= led_min_posiiton) {
+    ledBrightness = LED_CURVE[0];
   } else {
-    int brightness = getLedBrightness(newPosition);
-    setLeds(brightness, LED_PINS, 12);
+    fraction = ((encoder_position - led_min_posiiton) / (led_max_positon - led_min_posiiton));
+    int index = fraction * ledMaxBrghtness;
+    ledBrightness = LED_CURVE[index];
   }
-  
-  if (TAP_STATE == LOW && ((millis() - LAST_DEBOUNCE_TIME) > DEBOUCE_DELAY)){
-    sendMidi();
-    TAP_STATE = HIGH;
-    LAST_DEBOUNCE_TIME = millis();
-  }
-  
+  analogWrite(pin, ledBrightness);
 }
 
-int getLedBrightness(int midiValue) {
-  int ledMax = 255;
-  int midiMax = 127;
-  return (midiValue / 127.0) * ledMax;
-}
-
-void setLeds(int brightness, int pins[], int size) {
-  for (int idx=0; idx < size; idx++) {
-    int pin = pins[idx];
-    Serial.println(brightness);
-    analogWrite(pin, brightness);
+void updateEncoderPosition() {
+  long newPosition = ENCODER.read();
+  if (newPosition != ENCODER_POSITION) {
+    if (newPosition > ENCODER_MAX_POSITION) {
+      ENCODER.write(ENCODER_MAX_POSITION);
+      newPosition = ENCODER_MAX_POSITION;
+    } else if (newPosition < ENCODER_MIN_POSITION) {
+      ENCODER.write(ENCODER_MIN_POSITION);
+      newPosition = ENCODER_MIN_POSITION;
+    }
+    ENCODER_POSITION = newPosition;
   }
 }
 
-void tap(){
-  TAP_STATE = LOW;
+void intializePins () {
+  pinMode(LED_SAVE_PIN, OUTPUT);
+  for (int idx = 0; idx < LED_PARAM_PINS_LENGTH; idx++) {
+    pinMode(LED_PARAM_PINS[idx], OUTPUT);
+  }
+  for (int idx = 0; idx < LED_ENCODER_PINS_LENGTH; idx++) {
+    pinMode(LED_ENCODER_PINS[idx], OUTPUT);
+  }
+  pinMode(PARAM_BUTTON_PIN, INPUT_PULLUP);
 }
 
-
-void sendMidi() {
-  Serial.write(0xB0); // cc Midi Command 
-  Serial.write(114); // cc Parameter - Tap Tempo A
-  Serial.write(10); // cc Value - any value should work
+void turnOnLeds () {
+  digitalWrite(LED_SAVE_PIN, HIGH);
 }
-
